41d8f9311f53379beb09aa95368fb9b3f8ee93bc
Scott Murphy <scottmurphy09@gmail.com>
2014-05-15 09:26:51 -0500

    Ready for packaging

---
+++ plow-extras-lens/changelog.md
@@ -0,0 +1,24 @@
+# name: changelog
+# key: newchangelog
+# -- 
+
+Change Log
+==================
+
+
+#History Of Changes
+=================
+
+*For complete history please view commit log*
+
+##Current Version 0.1.0
+--------------------------	
+
+
+
+
+*Change History* 
+
++ 0.1.0
+  Added icatPrisms and catpPrisms
+
--- plow-extras-lens/src/Plow/Extras/Lens.hs
+++ plow-extras-lens/src/Plow/Extras/Lens.hs
@@ -1,18 +1,25 @@
-{-# LANGUAGE NoImplicitPrelude, NoMonomorphismRestriction #-}
+{-# LANGUAGE NoImplicitPrelude, NoMonomorphismRestriction, FlexibleContexts #-}
 
 module Plow.Extras.Lens where
 
 
 import Control.Category
 import Data.Monoid
-import Plow.Extras.Lens.Internal
-
+-- import Plow.Extras.Lens.Internal
+import Data.Functor
+import Control.Monad.Reader
 import Control.Lens 
-
-
+import Data.Foldable
+import Prelude (Int)
 -- |catPrisms l f  =  (some prism to a sumtype) -> (insertion funciton of the form (\a -> f monoid-container) ) -> container of only that sumtype constructor value 
 -- > x = Set.fromList [Left 1 ,Right 3, Left 4]
 -- > y = icatPrisms (_Just ) (Set.insert) x ==> [1,2,3]
+
+
+catPrisms :: (Functor (p a), Foldable f, Monoid r, Monoid a2,
+                    MonadReader (f a1) m, Profunctor p,
+                    Indexable Int p1) =>
+                   (p a (Const r a) -> p1 a1 (Const r a1)) -> p a (a2 -> r) -> m r
 
 catPrisms l f = views (folded.l) (f ?? mempty)
 
@@ -22,6 +29,11 @@ catPrisms l f = views (folded.l) (f ?? mempty)
 -- > xmap =  Map.fromList [(2,Just "fours"),(1,Just "thres")]
 -- > ymap =  icatPrisms (_Just) (Set.insert) x ==> fromList [(1,"thres"),(2,"fours")]
 
+
+icatPrisms :: (Functor f, Monoid b, Monoid a, Indexable i1 p, FoldableWithIndex i1 f1,
+                     MonadReader (f1 a2) f) =>
+                    (Indexed i a1 (Const (a -> b) a1) -> p a2 (Const (a -> b) a2))
+                    -> (i -> a1 -> a -> b) -> f b
 
 icatPrisms l f = iviews (ifolded . l )  f ?? mempty
 
