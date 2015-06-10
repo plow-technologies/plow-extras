aec6efec9cd6559d28544dc38e5ce114e2c4965a
Darren Midkiff <d.claymidkiff@gmail.com>
2015-06-10 16:36:27 -0500

    added more crontab tests

--- plow-extras-time/test/Main.hs
+++ plow-extras-time/test/Main.hs
@@ -21,18 +21,31 @@ timeTests = testGroup "plow-extras-time"
   ]
 
 cronTests = testGroup "plow-extras-crontab"
-  [ testCase "all asteriks" $
+  [ testCase "all asteriks 1" $
       testParser cron "* * * * *" @?= [(True,"")]
-  , testCase "all ranges - true" $
+  , testCase "all ranges 1 - true" $
       testParser cron "0-59 0-23 1-31 1-12 0-6" @?= [(True,"")]
-  , testCase "all ranges - false" $
+  , testCase "all ranges 1 - false" $
       testParser cron "1-59 1-23 2-31 2-12 2-6" @?= [(False,"")]
-  , testCase "asteriks, ranges, and ints - true" $
+  , testCase "asteriks, ranges, and ints 1 - true" $
       testParser cron "* 0-23 * 1 0" @?= [(True,"")]
-  , testCase "asteriks, ranges, and ints - false" $
+  , testCase "asteriks, ranges, and ints 1 - false" $
       testParser cron "5-10 * * 4 2" @?= [(False,"")]
-  , testCase "imposible range" $
-      testParser cron "5-10 * * 4 2" @?= [(False,"")]
+  , testCase "imposible range 1" $
+      testParser cron "5-3 * * 4 2" @?= [(False,"")]
+  , testCase "all asteriks 2" $
+      testParser cronTwo "* * * * *" @?= [(True,"")]
+  , testCase "all ranges 2 - true" $
+      testParser cronTwo "0-10 2-9 13-30 1-12 0-6" @?= [(True,"")]
+  , testCase "all ranges 2 - false" $
+      testParser cronTwo "1-10 1-23 2-31 5-12 2-6" @?= [(False,"")]
+  , testCase "asteriks, ranges, and ints 2 - true" $
+      testParser cronTwo "* 0-23 13 3 *" @?= [(True,"")]
+  , testCase "asteriks, ranges, and ints 2 - false" $
+      testParser cronTwo "5-10 * * 4 2" @?= [(False,"")]
+  , testCase "imposible range 2" $
+      testParser cronTwo "5-10 * * 4-1 2" @?= [(False,"")]
   ]
   where
     cron = CronTab 0 0 1 January Sunday
+    cronTwo = CronTab 5 4 13 March Wednesday
