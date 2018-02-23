9ad844554c3fdf3964d9a5114b167db8327e0c0a
Daniel Casanueva <dhelta.diaz@gmail.com>
2018-02-23 17:37:28 +0100

    plow-extras-base (#4)
    * plow-extras-base: Initial commit
    
    * EitherPartition. Initial code.
    
    * instance Bitraversable EitherPartition

---
+++ plow-extras-base/Plow/Extras/Base.hs
@@ -0,0 +1,70 @@
+
+-- | Types and functions that extend the 'base' package.
+--
+--   Please, do not add new dependencies without consent!
+--
+module Plow.Extras.Base (
+    -- * Either Partition
+    EitherPartition (..)
+  , foldEitherPartition
+  , foldMapEitherPartition
+  ) where
+
+import Data.Monoid (Endo (..))
+import Data.Bifunctor (Bifunctor (..))
+import Data.Bifoldable (Bifoldable (..))
+import Data.Bitraversable (Bitraversable (..))
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- EitherPartition
+
+-- | Result of combining a collection of values of type 'Either'.
+data EitherPartition err v = EitherPartition
+  { eitherPartitionLeft  :: err
+  , eitherPartitionRight :: v
+    }
+
+-- | Fold a foldable value with values of type 'Either'.
+foldEitherPartition
+  :: Foldable f
+  => (l -> l' -> l') -- ^ How to combine left values
+  -> (r -> r' -> r') -- ^ How to combine right values
+  -> EitherPartition l' r' -- ^ Initial partition
+  -> f (Either l r)
+  -> EitherPartition l' r'
+foldEitherPartition lf rf p0 = flip appEndo p0 . foldMap (
+  \e -> Endo $
+    \p -> case e of
+      Left  l -> p { eitherPartitionLeft  = lf l $ eitherPartitionLeft  p }
+      Right r -> p { eitherPartitionRight = rf r $ eitherPartitionRight p }
+  )
+
+-- | Transform left and right values to monoid values, then combine the results.
+foldMapEitherPartition
+  :: (Foldable f, Monoid l', Monoid r')
+  => (l -> l') -> (r -> r')
+  -> f (Either l r)
+  -> EitherPartition l' r'
+foldMapEitherPartition lf rf = foldEitherPartition (mappend . lf) (mappend . rf) mempty
+
+-- Instances
+
+instance Functor (EitherPartition l) where
+  fmap = second
+
+instance Bifunctor EitherPartition where
+  bimap  f g (EitherPartition l r) = EitherPartition (f l) (g r)
+  first  f   (EitherPartition l r) = EitherPartition (f l)    r
+  second f   (EitherPartition l r) = EitherPartition    l  (f r)
+
+instance (Monoid l, Monoid r) => Monoid (EitherPartition l r) where
+  mempty = EitherPartition mempty mempty
+  mappend (EitherPartition l r) (EitherPartition l' r') =
+    EitherPartition (mappend l l') (mappend r r')
+
+instance Bifoldable EitherPartition where
+  bifoldMap f g (EitherPartition l r) = mappend (f l) (g r)
+
+instance Bitraversable EitherPartition where
+  bitraverse f g (EitherPartition l r) = EitherPartition <$> f l <*> g r
